a1f1d74850b906e5d33043f01fab09357feeb11b
kcccraju <kcccraju@gmail.com>
2021-01-27 00:37:01 -0600

    Removed .idea files

--- target/classes/schema.sql
+++
@@ -1,9 +0,0 @@
-drop table if exist tbl_geolocation;
-
-create table tbl_geolocation (
-    id INTEGER NOT NULL AUTO_INCREMENT,
-    timeDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    ipAddress VARCHAR(16) NOT NULL,
-    location VARCHAR(100),
-    PRIMARY KEY (id)
-);
